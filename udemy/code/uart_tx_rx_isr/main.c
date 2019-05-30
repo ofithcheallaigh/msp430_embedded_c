47c6be47ea41c05d0fc684968d78550c55d62bc5
ofithcheallaigh <sofithcheallaigh@gmail.com>
2019-05-30 07:35:38 +0100

    Updateing UART programs
    Some example UART Tx Rx programs. ISR included in one.

--- udemy/code/uart_tx_rx_isr/main.c
+++ udemy/code/uart_tx_rx_isr/main.c
@@ -1,22 +1,3 @@
-/*
- * After we have loaded 0x56 into the UCA0TXBUF register, the program begins trying to receive
- * a UART message.
- *
- * When a UART message has been successfully received, the UART peripheral will set the
- * Universal Communication receive (RX) Interrupt FlaG (UCRXIFG) HI in the Universal
- * Communication interface, type A, number 0, Interrupt FlaG register (UCA0IFG). Therefore,
- * the program will continuously poll the UXRXIFG bit in the UCA0IFG register using
- * an if-statement
- *
- * With the long register names, this relatively simple operation can seem more complex.
- * Here it is with just the register names
- *      When the UART message has been successfully received, the UART peripheral
- *      will set the UCRXIFG bit HI in the UCA0IFG register. Therefore, the program
- *      will continuously poll the UXRXIFG bit in the UCA0IFG register using an
- *      if-statement
- */
-
-
 #include <msp430.h> 
 
 #define ENABLE_PINS             0xFFFE
@@ -42,36 +23,44 @@ main()
     assign_pins_to_uart();                  // P4.2 is for the TXD, P4.3 is for the RXD
     use_9600_baud();                        // UART operates at 9600 bits per second
 
+    /*
+     * Now we will look at using an ISR with the UART so the program so the program does not have to
+     * keep polling, or checking, to see if the UCRXIFG has gone HI
+     *
+     * To do this, we need to add two lines, shown below. First we need to enable the UART
+     * receive interrupt. Second, we activate the enabled interrupt
+     *
+     * After we send the message, we immediately put the program into an infinite loop to
+     * wait for the interrupt service routine
+     */
+    UCA0IE = UCRXIE;                        // Enable UART RXD interrupt
+    _BIS_SR(GIE);                           // Activate UART RXD interrupt
+
     UCA0TXBUF = 0x56;                       // This is our UART message to send out on P4.2
 
-    /*
-     * The code below shows what the program will do when a UART message is received (by having
-     * the UCRXIFG bit in the UCA0IFG register).
-     *
-     * When a UART byte is successfully received, the peripheral automatically stores the received
-     * byte in the UCA0RXBUF register. The program checks to see what the message is. If the message is
-     * 0x56, the program will turn on the red LED. If the message is anything else, the program
-     * turns off the red LED.
-     *
-     * Finally, the program clears the UCEXIFG bit in the UCA0IFG register so the program can begin looking
-     * for another UART message.
-     */
-    while(1)                                // Check for incoming message
-    {
-        if(UCA0IFG & UCRXIFG)               // Received any new messages?
-        {
-            if(UCA0RXBUF == 0x56)             // If the message is 0x56
-            {
-                P1OUT = BIT0;               // Turn the red LED on
-            }
-            else                            // Else, if the message is not 0x56
-            {
-                P1OUT = 0x00;               // Turn off the red LED
-            }
-            UCA0IFG = UCA0IFG & (~UCRXIFG); // Reset the UART receive flag
-        }
-    } // End while(1)
+    while(1);                               // Check for incoming message
+
 } // End main()
+
+/*
+ * ISR
+ */
+#pragma vector=USCI_A0_VECTOR
+__interrupt void UART_ISR(void)
+{
+    if(UCA0IFG & UCRXIFG)               // Received any new messages?
+    {
+        if(UCA0RXBUF == 0x56)             // If the message is 0x56
+        {
+            P1OUT = BIT0;               // Turn the red LED on
+        }
+        else                            // Else, if the message is not 0x56
+        {
+            P1OUT = 0x00;               // Turn off the red LED
+        }
+        UCA0IFG = UCA0IFG & (~UCRXIFG); // Reset the UART receive flag
+    }
+}
 
 void select_clock_signals(void)
 {
